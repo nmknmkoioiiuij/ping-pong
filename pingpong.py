92e81046ce2c1b1a9373f907a027d1c5a40e4a61
nmknmkoioiiuij <165478838+nmknmkoioiiuij@users.noreply.github.com>
2024-04-13 17:34:15 +0500

    Update pingpong.py

--- pingpong.py
+++ pingpong.py
@@ -1,8 +1,9 @@
 from pygame import *
 from random import randint
-from time import time as timer
 
-clock1 = time.Clock()
+
+
+
 
 
 FPS = 120
@@ -12,9 +13,9 @@ win_width = 700
 num_fire = 0
 
 class GameSprite(sprite.Sprite):
-    def __init__(self, player_image, player_x, player_y, player_speed):
+    def __init__(self, player_image, player_x,player_y,player_speed, widht,height):
         super().__init__()
-        self.image = transform.scale(image.load(player_image), (65, 65))
+        self.image = transform.scale(image.load(player_image), (widht,height))
         self.speed = player_speed
         self.rect = self.image.get_rect()
         self.rect.x = player_x
@@ -24,31 +25,42 @@ class GameSprite(sprite.Sprite):
 
 
 class Player(GameSprite):
-    def update(self):
-        keys_pressed = key.get_pressed()
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_height - 80:
+            self.rect.y -= self.speed
 
-        if keys_pressed[K_LEFT] and self.rect.x > 5:
-            self.rect.x  -= self.speed
-        if keys_pressed[K_RIGHT] and self.rect.x  < 595:
-            self.rect.x  += self.speed
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height - 80:
+            self.rect.y -= self.speed
 
-class Enemy(GameSprite):
-    def update(self):
-        self.rect.y += self.speed
-        global lost
-        if self.rect.y > win_height:
-            self.rect.x = randint(50 ,650)
-            self.rect.y = 0
-            lost = lost + 1
+back = (235, 103, 52)
+win_height = 500
+win_width = 700
 
-       
+
+game = True
+finish = False
+clock = time.Clock()
+fps = 120
+
+
+racket1 = Player('racket.png',)
+racket2 = Player('racket.png',)
 
 
 
 
 font.init()
 font1 = font.Font('Arial', 36)
-
+font1 = font.Font('Arial', 36)
+lose1 = font1.render('PLAYER 1 LOSE!', True, (180,0,0))
+lose2 = font2.render('PLAYER 2 LOSE!', True, (180,0,0))
 finish = False
 game = True
 
@@ -63,9 +75,21 @@ display.set_caption('1234')
 
 FPS = 120 
 score = 0
- 
+speed_x = 3
+speed_y = 3
 while game:
+    if finish != True:
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+
+    if ball.rect.y > win_height-50 or ball.rect.y < 0:
+        speed_y *= -1
+
+    if ball.rect.x < 0:
+        finish = False
+        window.blit(lose1, (200, 200))
+        window.blit(lose2, (200, 200))
     #window.blit(background, (0, 0))
 
-    clock1.tick(FPS)
+    clock.tick(FPS)
     display.update()
